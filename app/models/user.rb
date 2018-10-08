7bf0c47ef23101f2da96ddacc4de133f7b77ff9d
DharaRioja <cuatita_16@hotmail.com>
2018-10-08 13:00:22 -0400

    no sube imagenes de nit y ci

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,4 @@ class User < ApplicationRecord
   validates_attachment_content_type :imageci, content_type: /\Aimage\/.*\z/
   has_attached_file :imagenit, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :imagenit, content_type: /\Aimage\/.*\z/
-
-  # before_save :ensure_authentication_token
-  #
-  # def ensure_authentication_token
-  #     if authentication_token.blank?
-  #         self.authentication_token = generate_authentication_token
-  #     end
-  # end
-  #
-  # private
-  #  def generate_authentication_token
-  #    loop do
-  #      token = Devise.friendly_token
-  #      break token unless User.where(authentication_token: token).first
-  #    end
-  # end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :name, :lastname, :email, :ci, :phone, :nit, :address, :imagenit, :imageci, :password,
   :password_confirmation
+
+  def imagenit
+    if object.imagenit.present?
+      {
+        filename: object.imagenit_file_name
+      }
+    end
+  end
 end
