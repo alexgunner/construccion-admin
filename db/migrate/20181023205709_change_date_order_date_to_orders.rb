8ecc6f509d45c25be43c4fbd4f98ca993de681fe
DharaRioja <cuatita_16@hotmail.com>
2018-11-03 23:05:25 -0400

    migracion descomentada

--- db/migrate/20181023205709_change_date_order_date_to_orders.rb
+++ db/migrate/20181023205709_change_date_order_date_to_orders.rb
@@ -1,13 +1,13 @@
 class ChangeDateOrderDateToOrders < ActiveRecord::Migration[5.1]
-  # def up
-  #   change_table :orders do |t|
-  #     t.change :orderdate, :datetime
-  #   end
-  # end
-  #
-  # def down
-  #   change_table :orders do |t|
-  #     t.change :orderdate, :string
-  #   end
-  # end
+  def up
+    change_table :orders do |t|
+      t.change :orderdate, :datetime
+    end
+  end
+
+  def down
+    change_table :orders do |t|
+      t.change :orderdate, :string
+    end
+  end
 end
