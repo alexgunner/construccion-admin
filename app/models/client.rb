e0c8cd3ab7d6f45326bb661dd4ebaacb8ee5d2bd
DharaRioja <cuatita_16@hotmail.com>
2019-01-21 11:12:44 -0400

    exporta clientes falta poder filtrar

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,11 @@
 class Client < ApplicationRecord
   has_many :orders
+
+  def self.search(search)
+    if search
+      User.where('role LIKE ?', "%#{search}%")
+    else
+      Client.all
+    end
+  end
 end
