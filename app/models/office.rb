ddbb7d337f615ad84afcf4d0a34559b926c1a0ab
Dhara Rioja <cuatita_16@hotmail.com>
2019-03-05 02:17:27 -0400

    proceso compra completo con todos los filters en order

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,4 +1,8 @@
 class Office < ApplicationRecord
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  def name_with_city
+    "#{name} - #{city}"
+  end
 end
